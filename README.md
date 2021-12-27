48d2a0d67c377fcd377d3610da4d84e356b19032
Lastor-Chen <lastoraya@gmail.com>
2021-12-27 18:14:24 +0800

    [加入] Try to add static image and Bootstrap from CDN

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+# Learn Next.js
+This is a learning project refer to tutorial of [Next.js learn page](https://nextjs.org/learn).
+
+Next.js 純學習專案，參考官網教學文件。<br />
+嘗試添加 TS, Sass 等, 關切的內容。
--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
 
 console.log('firstPost init')
 
@@ -16,8 +18,21 @@ const FirstPost: NextPage = function() {
 
   return (
     <>
+      <Head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
+      </Head>
+
       <h1>First Post</h1>
-      <button onClick={() => setIsClicked(true)}>Click</button>
+      <div className="mb-3">
+        <button onClick={() => setIsClicked(true)}>Click</button>
+      </div>
+      <Image
+        src="/images/profile.jpg"
+        width="144"
+        height="144"
+        alt=""
+      />
       <h2>
         <Link href="/">
           <a>Back to home</a>
