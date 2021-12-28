c8e3c793da6e35044e452cf3c51867cb2dfe1cf7
Lastor-Chen <lastoraya@gmail.com>
2021-12-28 18:45:14 +0800

    [加入] 完成、調整 Polishing Layout 章節, 引入 lib react-if

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,87 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import style from './layout.module.scss'
+import utilStyle from '@styles/utils.module.scss'
+import { If, Then, Else } from 'react-if'
 
-const Layout: NextPage = function({ children }) {
+interface LayoutProps {
+  isHome?: boolean
+}
+
+const name = 'Name'
+export const siteTitle = 'Next.js Sample Website'
+
+const Layout: NextPage<LayoutProps> = function ({ children, isHome }) {
+  console.log('isHome', isHome)
   return (
-    <>
-      <div className={`container ${style.layout}`}>{children}</div>
-    </>
+    <div className={`${style.layout}`}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Learn how to build a personal website using Next.js" />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+
+      <header className={style.header}>
+        <If condition={isHome}>
+          <Then>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyle.borderCircle}
+              placeholder="blur"
+              blurDataURL="https://via.placeholder.com/10"
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className={utilStyle.heading2Xl}>{name}</h1>
+          </Then>
+          <Else>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={utilStyle.borderCircle}
+                  placeholder="blur"
+                  blurDataURL="https://via.placeholder.com/10"
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyle.headingLg}>
+              <Link href="/">
+                <a className={utilStyle.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </Else>
+        </If>
+
+        <hr className="w-100" />
+        <main>{children}</main>
+
+        <If condition={!isHome}>
+          <Then>
+            <div className={style.backToHome}>
+              <Link href="/">
+                <a>← Back to home</a>
+              </Link>
+            </div>
+          </Then>
+        </If>
+      </header>
+    </div>
   )
 }
 
--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next'
 import { useState, useEffect } from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '@components/layout'
 
 console.log('firstPost init')
@@ -10,11 +8,11 @@ console.log('firstPost init')
 const FirstPost: NextPage = function() {
   const [isClicked, setIsClicked] = useState(false)
   console.log('\ninvoke FirstPost')
-  console.log(`isClicked: ${isClicked}`)
+  console.log('isClicked:', isClicked)
 
   useEffect(() => {
     console.log('side effect')
-    console.log(`isClicked: ${isClicked}`)
+    console.log('isClicked:', isClicked)
   })
 
   return (
@@ -24,20 +22,10 @@ const FirstPost: NextPage = function() {
       </Head>
 
       <h1>First Post</h1>
-      <div className="mb-3">
-        <button onClick={() => setIsClicked(true)}>Click</button>
+      <div className="mb-3 text-center">
+        <button className='btn btn-primary' onClick={() => setIsClicked(!isClicked)}>Click</button>
       </div>
-      <Image
-        src="/images/profile.jpg"
-        width="144"
-        height="144"
-        alt=""
-      />
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+
     </Layout>
   )
 }
